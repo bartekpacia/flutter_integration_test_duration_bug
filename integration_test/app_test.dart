416ab63e64ac7df4ca75f66087b654ea6442cefd
Bartek Pacia <barpac02@gmail.com>
2022-12-20 13:13:41 +0100

    use test group to demo another bug

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -6,20 +6,25 @@ import 'package:integration_test/integration_test.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+  group('counter', () {
+    testWidgets(
+      'is incremented when plus button is tapped',
+      (WidgetTester tester) async {
+        await tester.pumpWidget(const MyApp());
 
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+        expect(find.text('0'), findsOneWidget);
+        expect(find.text('1'), findsNothing);
 
-    await tester.pump(const Duration(seconds: 3));
+        await tester.pump(const Duration(seconds: 3));
 
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+        await tester.tap(find.byIcon(Icons.add));
+        await tester.pump();
 
-    await tester.pump(const Duration(seconds: 3));
+        await tester.pump(const Duration(seconds: 3));
 
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+        expect(find.text('0'), findsNothing);
+        expect(find.text('1'), findsOneWidget);
+      },
+    );
   });
 }
